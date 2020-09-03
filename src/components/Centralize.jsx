dc480db41cdb12783769fa6fc925fe7ea0d26151
salihcodev <ahmedsalihdev@gmail.com>
2020-09-04 00:59:45 +0200

    Setup file structure, necessary dependancies :)

---
+++ src/components/Centralize.jsx
@@ -0,0 +1,11 @@
+// import all files to export from centralize location:
+
+import Info from "./info/Info";
+import Repos from "./repositories/Repos";
+import User from "./user/User";
+import Search from "./search/Search";
+import Header from "./header/Header";
+import Card from "./header/Header";
+import Followers from "./header/Header";
+
+export { Info, Repos, User, Search, Header, Card, Followers };
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components/Centralize";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/Context";
+const Dashboard = () => {
+  return (
+    <main>
+      <h2>Dashboard Page</h2>
+    </main>
+  );
+};
+
+export default Dashboard;
