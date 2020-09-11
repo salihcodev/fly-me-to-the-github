3067130d360df2c7d78e7b23f46a21a410a3c764
salihcodev <ahmedsalihdev@gmail.com>
2020-09-12 00:21:46 +0200

    Redesigning <LoginPage> :paintbrush:

--- src/pages/login/Login.page.jsx
+++ src/pages/login/Login.page.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
-import loginImg from "../../images/login-img.svg";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import LoginAside from "../../assets/login-aside.svg";
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
+
   return (
-    <h2>
-      <button onClick={loginWithRedirect}>login / sign up</button>
-    </h2>
+    <LoginPageWrapper className="flex items-center">
+      <aside style={{ backgroundImage: ` url(${LoginAside})` }}></aside>
+      <div className="login-container flex justify-center">
+        <button
+          onClick={loginWithRedirect}
+          title="login / signup"
+          className="border-2 border-gray-700 bg-orange-200 py-4 px-6 flex justify-center font-bold"
+        >
+          <span className="text">Fly on github</span>
+          <span className="icon pl-4 pt-1">{FaLongArrowAltRight()} </span>
+        </button>
+      </div>
+    </LoginPageWrapper>
   );
 };
 
 export default Login;
+
+const LoginPageWrapper = styled.main`
+  aside {
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    width: 40rem;
+  }
+
+  .login-container {
+    width: 50%;
+  }
+`;
