4dac6e340c96d98f431cd50169d057e1b898e2d3
salihcodev <ahmedsalihdev@gmail.com>
2020-10-01 23:05:40 +0200

    Update footer links.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { AiFillHeart } from "react-icons/ai";
-import { GiCoffeeCup } from "react-icons/gi";
-import { FiAtSign } from "react-icons/fi";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { AiFillHeart } from 'react-icons/ai';
+import { GiCoffeeCup } from 'react-icons/gi';
+import { FiAtSign } from 'react-icons/fi';
 
 const Footer = () => {
   return (
@@ -21,13 +21,13 @@ const Footer = () => {
               <GiCoffeeCup />
             </span>
             Menufia, Egypt by
-            <Link
-              to="https://twitter.com/salihcodev"
+            <a
+              href="https://twitter.com/salihcodev"
               target="_blank"
               className="text-teal-800 ml-1 font-semibold"
             >
               Ahmed Salih
-            </Link>
+            </a>
           </p>
         </div>
 
@@ -35,15 +35,17 @@ const Footer = () => {
         <div className="copyrights flex-grow text-right">
           <p className="copyrights-policy text-gray-700 text-sm">
             {/* current year */}
-            <span className="current-year mr-1">@{new Date().getFullYear()}</span>
+            <span className="current-year mr-1">
+              @{new Date().getFullYear()}
+            </span>
             Allrights reserved to
-            <Link
-              to="https://github.com/salihcodev"
+            <a
+              href="https://github.com/salihcodev"
               target="_blank"
               className="text-teal-800 ml-1 hover:border-gray-400 border-b"
             >
               @salihcodev
-            </Link>
+            </a>
           </p>
         </div>
       </main>
